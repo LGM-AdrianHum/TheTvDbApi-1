9db5a6df119f25f4ea5f907428ae3569c76911b6
Adrian Hum <adrian@littlegreenmartian.net>
2017-10-23 15:38:08 +1100

    Initial Updates For Observable and Mvvm class

---
+++ TheTvDbApi.Mvvm/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TheTvDbApi.Mvvm.Annotations;
+
+namespace TheTvDbApi.Mvvm
+{
+    public class TheTvDbApiVm : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+         
+        
+        public void AssociateFileListWithSeasonEpisodeCollection(List<string> fileList)
+        {
+            
+        }
+    }
+
+    public class FileToSeason
+    {
+        public string SeasonKey { get; set; } 
+        public List<FileInfo> FileInfoList { get; set; }=new List<FileInfo>();
+    }
+    
+}
--- TheTvDbApi.Observable/BaseObservable.cs
+++ TheTvDbApi.Observable/BaseObservable.cs
@@ -1,8 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//  _____ _        _____    ______ _      ___        _   
+// |_   _| |      |_   _|   |  _  \ |    / _ \      (_)  
+//   | | | |__   ___| |_   _| | | | |__ / /_\ \_ __  _   
+//   | | | '_ \ / _ \ \ \ / / | | | '_ \|  _  | '_ \| |  
+//   | | | | | |  __/ |\ V /| |/ /| |_) | | | | |_) | |_ 
+//   \_/ |_| |_|\___\_/ \_/ |___/ |_.__/\_| |_/ .__/|_(_)
+//                                            | |        
+//                                            |_|        
+// 
+// Module   : TheTvDbApi/TheTvDbApi.Observable/BaseObservable.cs
+// Name     : Adrian Hum - adrianhum 
+// Created  : 2017-10-23-3:15 PM
+// Modified : 2017-10-23-3:25 PM
 
 namespace TheTvDbApi.Observable
 {
