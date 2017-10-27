df839d845b0d88f3b10499c880fbbf940eff033c
Adrian Hum <adrian@littlegreenmartian.net>
2017-10-28 09:12:34 +1100

    Bug: Quantum Leap 72248, found DvdEpisodeNumber = 1.1 (changed data type to decimal)

--- TheTvDbApi/Series/DataTypes/EpisodeInfo.cs
+++ TheTvDbApi/Series/DataTypes/EpisodeInfo.cs
@@ -1,9 +1,26 @@
-﻿namespace TheTvDbApi.Series.DataTypes {
-    public class EpisodeInfo {
+﻿//  _____ _        _____    ______ _      ___        _   
+// |_   _| |      |_   _|   |  _  \ |    / _ \      (_)  
+//   | | | |__   ___| |_   _| | | | |__ / /_\ \_ __  _   
+//   | | | '_ \ / _ \ \ \ / / | | | '_ \|  _  | '_ \| |  
+//   | | | | | |  __/ |\ V /| |/ /| |_) | | | | |_) | |_ 
+//   \_/ |_| |_|\___\_/ \_/ |___/ |_.__/\_| |_/ .__/|_(_)
+//                                            | |        
+//                                            |_|        
+// File: TheTvDbApi/TheTvDbApi/EpisodeInfo.cs
+// User: Adrian Hum/
+// 
+// Created:  2017-10-28 9:08 AM
+// Modified: 2017-10-28 9:09 AM
+
+//2017-10-28: Changed AiredEpisodeNumber and DvdEpisodeNumber to decimal (See Quantum Leap, 72248)
+namespace TheTvDbApi.Series.DataTypes
+{
+    public class EpisodeInfo
+    {
         public int AbsoluteNumber { get; set; }
-        public int AiredEpisodeNumber { get; set; }
+        public decimal AiredEpisodeNumber { get; set; }
         public int AiredSeason { get; set; }
-        public int DvdEpisodeNumber { get; set; }
+        public decimal DvdEpisodeNumber { get; set; }
         public int DvdSeason { get; set; }
         public string EpisodeName { get; set; }
         public int Id { get; set; }
