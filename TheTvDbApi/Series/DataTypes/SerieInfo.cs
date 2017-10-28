8fe79d44f3b2fcf7ad89716e0c3740bca6b9de47
Adrian Hum <adrian@littlegreenmartian.net>
2017-10-28 20:08:39 +1100

    Update to add readonly date time to Series Info for age.

--- TheTvDbApi/Series/DataTypes/SerieInfo.cs
+++ TheTvDbApi/Series/DataTypes/SerieInfo.cs
@@ -1,10 +1,29 @@
-﻿namespace TheTvDbApi.Series.DataTypes {
-    public class SerieInfo {
+﻿//  _____ _        _____    ______ _      ___        _   
+// |_   _| |      |_   _|   |  _  \ |    / _ \      (_)  
+//   | | | |__   ___| |_   _| | | | |__ / /_\ \_ __  _   
+//   | | | '_ \ / _ \ \ \ / / | | | '_ \|  _  | '_ \| |  
+//   | | | | | |  __/ |\ V /| |/ /| |_) | | | | |_) | |_ 
+//   \_/ |_| |_|\___\_/ \_/ |___/ |_.__/\_| |_/ .__/|_(_)
+//                                            | |        
+//                                            |_|        
+// File: RollThroughLibrary/TheTvDbApi/SerieInfo.cs
+// User: Adrian Hum/
+// 
+// Created:  2017-10-23 5:16 PM
+// Modified: 2017-10-28 9:39 AM
+
+using System;
+using TheTvDbApi.Updates;
+
+namespace TheTvDbApi.Series.DataTypes
+{
+    public class SerieInfo
+    {
         public int Id { get; set; }
         public string SeriesName { get; set; }
         public string[] Aliases { get; set; }
         public string Banner { get; set; }
-        public int? SeriesId { get; set; }
+        public string SeriesId { get; set; } // Please check this data against your collections, 
         public string Status { get; set; }
         public string FirstAired { get; set; }
         public string Network { get; set; }
@@ -12,11 +31,13 @@
         public string Runtime { get; set; }
         public string[] Genre { get; set; }
         public string Overview { get; set; }
-        
+
         /// <summary>
-        /// <see cref="TheTvDbApi.Updates.DataTypes.UpdatedSeries.LastUpdated"/>
-        /// Epoch datetime when this record was last updated.
+        ///     <see cref="UpdatedSeries.LastUpdated" />
+        ///     Epoch datetime when this record was last updated.
         /// </summary>
+        public DateTime LastUpdatedDateTime => LastUpdated.FromUnixTime();
+
         public long LastUpdated { get; set; }
         public string AirsDayOfWeek { get; set; }
         public string AirsTime { get; set; }
--- TheTvDbApi/TimeConverters.cs
+++ TheTvDbApi/TimeConverters.cs
@@ -1,15 +1,39 @@
-﻿using System;
+﻿//  _____ _        _____    ______ _      ___        _   
+// |_   _| |      |_   _|   |  _  \ |    / _ \      (_)  
+//   | | | |__   ___| |_   _| | | | |__ / /_\ \_ __  _   
+//   | | | '_ \ / _ \ \ \ / / | | | '_ \|  _  | '_ \| |  
+//   | | | | | |  __/ |\ V /| |/ /| |_) | | | | |_) | |_ 
+//   \_/ |_| |_|\___\_/ \_/ |___/ |_.__/\_| |_/ .__/|_(_)
+//                                            | |        
+//                                            |_|        
+// File: RollThroughLibrary/TheTvDbApi/TimeConverters.cs
+// User: Adrian Hum/
+// 
+// Created:  2017-10-28 9:12 AM
+// Modified: 2017-10-28 9:36 AM
+
+using System;
 
 namespace TheTvDbApi
 {
     public static class TimeConverters
     {
+        /// <summary>
+        /// Converts int64 (long) to DateTime
+        /// </summary>
+        /// <param name="unixTime"></param>
+        /// <returns></returns>
         public static DateTime FromUnixTime(this long unixTime)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             return epoch.AddSeconds(unixTime);
         }
 
+        /// <summary>
+        /// Converts Windows DateTime to long (Epoch Date)
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
         public static long ToUnixTime(this DateTime date)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
